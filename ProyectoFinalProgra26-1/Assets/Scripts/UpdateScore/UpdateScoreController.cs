590e28e6b8ac086eb3392689060b7c3733035eea
RaphaelXZA <108958428+RaphaelXZA@users.noreply.github.com>
2026-06-29 02:57:12 -0500

    Update Score

--- ProyectoFinalProgra26-1/Assets/Scripts/UpdateScore/UpdateScoreController.cs
+++ ProyectoFinalProgra26-1/Assets/Scripts/UpdateScore/UpdateScoreController.cs
@@ -1,16 +1,36 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class UpdateScoreController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private const string URL = "https://progra261chgrupo3.samidareno.com/update_score.php";
+
+    public void Send(int playerId, int score, Action<string> callback)
     {
-        
+        StartCoroutine(SendRequest(playerId, score, callback));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SendRequest(int playerId, int score, Action<string> callback)
     {
-        
+        WWWForm form = new WWWForm();
+        form.AddField("player_id", playerId);
+        form.AddField("score", score);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                callback?.Invoke(www.downloadHandler.text);
+            }
+            else
+            {
+                Debug.Log(www.error);
+            }
+        }
     }
+
 }
--- ProyectoFinalProgra26-1/Assets/Scripts/UpdateScore/UpdateScoreUI.cs
+++ ProyectoFinalProgra26-1/Assets/Scripts/UpdateScore/UpdateScoreUI.cs
@@ -1,16 +1,32 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UpdateScoreUI : MonoBehaviour
+public class UpdateScoreView : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TMP_InputField playerIdInputField;
+    [SerializeField] private TMP_InputField scoreInputField;
+    [SerializeField] private TextMeshProUGUI resultText;
+    [SerializeField] private Button button;
+    private UpdateScoreController controller;
+
+    private void Awake()
     {
-        
+        controller = GetComponent<UpdateScoreController>();
+        button.onClick.AddListener(Send);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Send()
     {
-        
+        int playerId = int.Parse(playerIdInputField.text);
+        int score = int.Parse(scoreInputField.text);
+
+        controller.Send(playerId, score, OnResult);
     }
+
+    private void OnResult(string result)
+    {
+        resultText.text = result;
+    }
+
 }
